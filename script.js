713a3e6fcb790ac248fbf5483e79fb402d8a1be2
Shreya-devs <shreyamuchiradka19@gmail.com>
2026-06-08 19:52:02 +0530

    Updated CSS and JavaScript

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ let hours=document.querySelector("#Hours");
 let minutes=document.querySelector("#Minutes");
 let seconds=document.querySelector("#Seconds");
 
-
 const Time=()=>{
-let Now=new Date();
-hours.innerText=Now.getHours();
-minutes.innerText=Now.getMinutes();
-seconds.innerText=Now.getSeconds();
-}
+    let Now=new Date();
+    hours.innerText=Now.getHours();
+    minutes.innerText=Now.getMinutes();
+    seconds.innerText=Now.getSeconds();
+};
 Time();
-setInterval(Time,1000);
+setInterval(Time(),1000)
