a54ec0db93894d0f5838cb7c0b185cce42577d7f
Shreya-devs <shreyamuchiradka19@gmail.com>
2026-06-08 19:40:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let hours=document.querySelector("#Hours");
+let minutes=document.querySelector("#Minutes");
+let seconds=document.querySelector("#Seconds");
+
+
+const Time=()=>{
+let Now=new Date();
+hours.innerText=Now.getHours();
+minutes.innerText=Now.getMinutes();
+seconds.innerText=Now.getSeconds();
+}
+Time();
+setInterval(Time,1000);
